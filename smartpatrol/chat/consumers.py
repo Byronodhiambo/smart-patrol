fc705578c70fae8e774e9c425dc195f43ce8b561
Byronodhiambo <byronjonnes25@gmail.com>
2021-10-07 19:20:01 +0300

    report

--- smartpatrol/chat/consumers.py
+++ smartpatrol/chat/consumers.py
@@ -1,22 +1,21 @@
-# when Channels accepts a WebSocket connection, it consults the root routing configuration to lookup a consumer, and then calls various functions on the consumer to handle events from the connection.
-# consumer that accepts WebSocket connections on the path /ws/chat/ROOM_NAME/ that takes any message it receives on the WebSocket and echos it back to the same WebSocket.
 import json
-# this enables the message to be seen on both sides of the room (in coordination with redis runing on docker)
-# docker is a container that we can run several applications at the same time(more like virtualization)
-# from asgiref.sync import async_to_sync
-# from channels.generic.websocket import WebsocketConsumer
-
 from channels.generic.websocket import AsyncWebsocketConsumer
+#from asgiref.sync import await_to_sync
+from chat.services import chat_save_message
 
 
-# this file as 3 methods
+
 class ChatConsumer(AsyncWebsocketConsumer):
-    # this first metod only initiates a connection
+    """ handshake websocket front end """
+
+    room_name = None
+    room_group_name = None
+
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
 
-         # Join room group
+        # Join room group
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
@@ -25,36 +24,54 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.accept()
 
 
-    async def disconnect(self, close_code):
+    async def disconnect(self, code):
         # Leave room group
-        await self.channel_layer.group_discard(
-            self.room_group_name,
-            self.channel_name
-        )
+        if self.room_group_name and self.channel_name:
+            await self.channel_layer.group_discard(
+                self.room_group_name,
+                self.channel_name
+            )
 
-
- # Receive message from WebSocket
-    async def receive(self, text_data):
-        # grab the textdata and convert into json
+    # Receive message from WebSocket
+    async def receive(self, text_data=None, bytes_data=None):
         text_data_json = json.loads(text_data)
-        # since json is a dictionary access the key and assign it to the message
         message = text_data_json['message']
+        image_caption = text_data_json['image_caption']
+        message_type = text_data_json['message_type']
 
         # Send message to room group
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'username': self.scope['user'].username.title(),
+                'message': message,
+                'message_type': message_type,
+                'image_caption': image_caption
             }
         )
 
+        await chat_save_message(
+            username=self.scope['user'].username.title(),
+            room_id=self.room_name,
+            message=message,
+            message_type=message_type,
+            image_caption=image_caption
+        )
 
-     # Receive message from room group
+
+    # Receive message from room group
     async def chat_message(self, event):
+        """ exhange message here """
         message = event['message']
+        username = event['username']
+        image_caption = event['image_caption']
+        message_type = event['message_type']
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
-            'message': message
+            'message': message,
+            'username': username,
+            'image_caption': image_caption,
+            'message_type': message_type
         }))
